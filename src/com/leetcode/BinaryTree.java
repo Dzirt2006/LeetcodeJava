b9f29107d03b4ab52a9ea888d5a7d55b5f5590d9
Dzirt2006 <dzirt-2006@mail.ru>
2020-10-12 13:01:56 -0400

    BT branch summ

--- src/com/leetcode/BinaryTree.java
+++ src/com/leetcode/BinaryTree.java
@@ -1,6 +1,8 @@
 package com.leetcode;
 
 
+import java.util.*;
+
 class TreeNode {
     int val;
     TreeNode left;
@@ -22,7 +24,7 @@ class TreeNode {
 
 public class BinaryTree {
     //new tree [3,9,20,null,null,15,7]
-    TreeNode mainTree = new TreeNode(3, new TreeNode(9), new TreeNode(2, new TreeNode(15), new TreeNode(7)));
+    TreeNode mainTree = new TreeNode(1, new TreeNode(2,new TreeNode(4,new TreeNode(8), new TreeNode(9)), new TreeNode(5,new TreeNode(10), null)), new TreeNode(3, new TreeNode(6), new TreeNode(7)));
 
 
     //-----------------------------------------------------------------Maximum Depth of Binary Tree-------------easy-------------------------------------------------------------
@@ -38,5 +40,33 @@ public class BinaryTree {
         }
     }
 
+    //----------------------------------------------------------------branch sums-----------------------------------------------
+    public static List<Integer> branchSums(TreeNode root) {
+        ArrayList<Integer> arr=new ArrayList<>();
+        BinaryTree.branchSumRecurcion(root,0,arr);
+        return arr;
+    }
+    static void branchSumRecurcion(TreeNode root,int value,List<Integer> arr){
+        value+=root.val;
 
+        System.out.println();
+        if(root.left==null && root.right==null){
+            System.out.println(value);
+            arr.add(value);
+        }else {
+            if (root.left != null) {
+                branchSumRecurcion(root.left,value,arr);
+            }
+            if (root.right != null) {
+                branchSumRecurcion(root.right,value,arr);
+            }
+        }
+    }
+}
+
+class RunnerBtree{
+    public static void main(String[] args) {
+        BinaryTree tree=new BinaryTree();
+        System.out.println(BinaryTree.branchSums(tree.mainTree));
+    }
 }
